43371ae8ea5f22655bca95cf73cb1242014b045b
TUMUKUNTA SRIVALLI <tumukuntasrivalli@gmail.com>
2026-07-08 11:49:41 +0530

    Update README and project features

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,12 +5,9 @@ import { useAuth } from "@/context/AuthContext";
 import SignOutButton from "@/components/SignOutButton";
 
 export default function Navbar() {
-
   const { user } = useAuth();
 
-
   return (
-
     <nav
       className="
       sticky
@@ -29,15 +26,9 @@ export default function Navbar() {
       shadow-xl
       "
     >
-
-
-
       {/* Brand */}
-
       <Link
-
         href="/dashboard"
-
         className="
         text-2xl
         font-extrabold
@@ -51,19 +42,11 @@ export default function Navbar() {
         hover:scale-105
         transition
         "
-
       >
-
         Civic Issue Reporting
-
       </Link>
 
-
-
-
-
       {/* Navigation Links */}
-
       <div
         className="
         flex
@@ -73,13 +56,9 @@ export default function Navbar() {
         font-medium
         "
       >
-
-
-
+        {/* Dashboard */}
         <Link
-
           href="/dashboard"
-
           className="
           rounded-lg
           px-3
@@ -89,76 +68,49 @@ export default function Navbar() {
           hover:text-cyan-400
           transition
           "
-
         >
-
           Dashboard
-
         </Link>
 
+        {/* Citizen Only */}
+        {user?.role === "citizen" && (
+          <>
+            <Link
+              href="/complaints/new"
+              className="
+              rounded-lg
+              px-3
+              py-2
+              text-zinc-300
+              hover:bg-zinc-800
+              hover:text-cyan-400
+              transition
+              "
+            >
+              Report Complaint
+            </Link>
 
-
-
-
-
-        <Link
-
-          href="/complaints/new"
-
-          className="
-          rounded-lg
-          px-3
-          py-2
-          text-zinc-300
-          hover:bg-zinc-800
-          hover:text-cyan-400
-          transition
-          "
-
-        >
-
-          Report Complaint
-
-        </Link>
-
-
-
-
-
-
-        <Link
-
-          href="/complaints"
-
-          className="
-          rounded-lg
-          px-3
-          py-2
-          text-zinc-300
-          hover:bg-zinc-800
-          hover:text-cyan-400
-          transition
-          "
-
-        >
-
-          My Complaints
-
-        </Link>
-
-
-
-
-
+            <Link
+              href="/complaints"
+              className="
+              rounded-lg
+              px-3
+              py-2
+              text-zinc-300
+              hover:bg-zinc-800
+              hover:text-cyan-400
+              transition
+              "
+            >
+              My Complaints
+            </Link>
+          </>
+        )}
 
         {/* Admin */}
-
         {user?.role === "admin" && (
-
           <Link
-
             href="/admin/dashboard"
-
             className="
             rounded-lg
             px-3
@@ -168,29 +120,15 @@ export default function Navbar() {
             hover:text-cyan-400
             transition
             "
-
           >
-
             Admin Dashboard
-
           </Link>
-
         )}
-
-
-
-
-
-
 
         {/* Officer */}
-
         {user?.role === "officer" && (
-
           <Link
-
             href="/officer/dashboard"
-
             className="
             rounded-lg
             px-3
@@ -200,29 +138,14 @@ export default function Navbar() {
             hover:text-cyan-400
             transition
             "
-
           >
-
             Officer Dashboard
-
           </Link>
-
         )}
-
-
-
       </div>
 
-
-
-
-
-
       {/* Sign Out */}
-
       <div className="ml-auto">
-
-
         <div
           className="
           rounded-xl
@@ -235,21 +158,9 @@ export default function Navbar() {
           transition
           "
         >
-
           <SignOutButton />
-
-
         </div>
-
-
       </div>
-
-
-
-
     </nav>
-
-
   );
-
 }
